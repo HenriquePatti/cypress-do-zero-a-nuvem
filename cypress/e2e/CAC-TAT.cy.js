77ae42e2b3a84988e630a5cdac017df36f753bb2
Henrique Patti <henriquepatti@Henriques-MacBook-Air.local>
2025-02-10 15:49:54 -0300

    test: preenche os campos obrigatórios e envia formulário
    - teste para validar o preenchimento de campos obrigatórios.
    - inclui o beforeEach() para acessar a url CAC-TAT antes do início dos testes.

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,19 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('verifica o título da aplicação', () => {
+ 
+  beforeEach(()=>{
     cy.visit('./src/index.html')
+  })
+  it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', ()=>{
+    cy.get('#firstName').type( 'Pomposo')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('pomposo-silva@gmail.com')
+    cy.get('#open-text-area').type('QA teste plataforma CAC-TAT')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
   })
 })
